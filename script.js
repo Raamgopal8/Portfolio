2fc1a629204be7dba69cde706f0513d4183db666
Raamgopal8 <raamgopal2369@outlook.com>
2024-10-13 20:41:06 +0530

    new

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
-let selections = document.querySelectorAll('section');
+let sections = document.querySelectorAll('section');
 let navLinks = document.querySelector('header nav a');
 
 window.onscroll = () =>{
@@ -8,18 +8,17 @@ window.onscroll = () =>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute;
+        let id = sec.getAttribute(id);
+
         if(top >= offset && top <offset + height){
             navLinks.forEach(links =>){
                 links.classList.remove('active');
             
-                document.querySelector('header nav a[href*='+id +']').classList.add('active')       
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')       
              }
         }
     })
 }
-
-
 
 menuIcon.onclick = () => {
     menuIcon.classList.toggle('bx-x');
