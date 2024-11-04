1019148292bb16d80c3bb2e9a8aa632c79cdd111
Raamgopal8 <raamgopal2369@outlook.com>
2024-11-04 17:11:11 +0530

    New

--- script.js
+++ script.js
@@ -1,21 +1,24 @@
+const { JSDOM } = require('jsdom');
+const dom = new JSDOM(`<!DOCTYPE html><p>Hello world</p>`);
+const document = dom.window.document;
 let menuIcon = document.querySelector('#menu-icon');
 let navbar = document.querySelector('.navbar');
 let sections = document.querySelectorAll('section');
 let navLinks = document.querySelector('header nav a');
+
 
 window.onscroll = () =>{
     selections.forEach(sec =>{
         let top = window.scrollY;
         let offset = sec.offsetTop - 150;
         let height = sec.offsetHeight;
-        let id = sec.getAttribute(id);
+        let id = sec.getAttribute('id');
 
-        if(top >= offset && top <offset + height){
-            navLinks.forEach(links =>){
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links =>{
                 links.classList.remove('active');
-            
-                document.querySelector('header nav a [href*=' + id + ' ]').classList.add('active')       
-             }
+                document.querySelector('header nav a [href*=' + id + ' ]').classList.add ('active')       
+             })
         }
     })
 }
---
+++ server.js
@@ -0,0 +1,50 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const path  = require('path')
+const port = 4000
+
+const app = express();
+app.use(express.static(__dirname));
+app.use(express.urlencoded({extended:true}))
+
+
+mongoose.connect('mongodb://localhost:27017/users')
+const db = mongoose.connection
+db.once('open',()=>{
+    console.log("mongodb connection success!")
+})
+
+const userSchema = new mongoose.Schema({
+  name: { type: String, required: true },
+  email: { type: String, required: true, match: /.+\@.+\..+/ }, // Basic email validation
+  PhoneNumber: { type: String, required: true, match: /^\d{10}$/ }, // Example for 10-digit phone number
+  Location: { type: String, required: true },
+  query: { type: String, required: true }
+})
+const Users = mongoose.model("data",userSchema)
+
+app.get('/',(req,res)=>{
+    res.sendFile(path.join(__dirname,'index.html'))
+})
+
+app.post('/post',async(req,res)=>{
+  
+  const {name,email,PhoneNumber,Location,query} = req.body;
+
+  const user = new Users({
+    name,
+    email,
+    PhoneNumber,
+    Location,
+    query
+  })
+  await user.save()
+  console.log(user)
+  res.send("Form Submitted");
+})
+
+
+app.listen(port,()=>{
+    console.log("Server is Running")
+})
